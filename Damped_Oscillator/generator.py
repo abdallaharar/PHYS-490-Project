9a3284a9595f34d16d23dce928c9744927984bc0
N0WaifuN0Lifu <ntfische@uwaterloo.ca>
2021-03-28 13:00:23 -0600

    Changed dumb line in generator.py

--- Damped_Oscillator/generator.py
+++ Damped_Oscillator/generator.py
@@ -24,6 +24,7 @@ class oscillator:
             self.y_points.append(x/self.a)
             i += self.time/self.iterations
     def show_underdamped(self):
+        print("debug1")
         self.iterate_underdamped()
         plt.plot(damped_oscillator.x_points[1:], damped_oscillator.y_points[1:])
         plt.plot(damped_oscillator.x_points[0:1], damped_oscillator.y_points[0:1])  
@@ -41,6 +42,7 @@ class oscillator:
             i += self.time/self.iterations
 
     def show_critically(self):
+        print("debug")
         self.iterate_critically()
         plt.plot(damped_oscillator.x_points[1:], damped_oscillator.y_points[1:])
         plt.plot(damped_oscillator.x_points[0:1], damped_oscillator.y_points[0:1])  
@@ -51,24 +53,24 @@ class oscillator:
 
 #Oscillator(Gamma Value (dampenning), A (maximum) value, Omega Value, Alpha Value, Time scale, Initial velocity, Iterations to plot)
 
-
-data = []
-for i in range(5):
-    Omega = random.uniform(5,10)
-    Alpha = random.uniform(5,10)
-    Velocity = random.uniform(-1,1)
-    if (i % 100) == 0:
-        print(i)
-    batch = []
-    Gamma = random.uniform(0.5,1)
-    print(Gamma, Omega, Alpha)
-    damped_oscillator = oscillator(Gamma, 1, Omega, Alpha, 10, Velocity, 100)
-    mylist = [damped_oscillator.show_critically,damped_oscillator.show_underdamped]
-    #damped_oscillator.show_critically()
-    random.choice(mylist)()
-    batch = [damped_oscillator.x_points[0:50],damped_oscillator.y_points[0:50]]
-    question = random.randint(0,100)
-    data.append([batch,[damped_oscillator.x_points[question],damped_oscillator.y_points[question]]])
-    
-with open('data.json', 'w', encoding='utf-8') as f:
-    json.dump(data, f, ensure_ascii=False, indent=4)
+if __name__ == __main__:
+    data = []
+    for i in range(5):
+        Omega = random.uniform(5,10)
+        Alpha = random.uniform(5,10)
+        Velocity = random.uniform(-1,1)
+        if (i % 100) == 0:
+            print(i)
+        batch = []
+        Gamma = random.uniform(0.5,1)
+        print(Gamma, Omega, Alpha)
+        damped_oscillator = oscillator(Gamma, 1, Omega, Alpha, 10, Velocity, 100)
+        mylist = [damped_oscillator.show_critically,damped_oscillator.show_underdamped]
+        #damped_oscillator.show_critically()
+        random.choice(mylist)()
+        batch = [damped_oscillator.x_points[0:50],damped_oscillator.y_points[0:50]]
+        question = random.randint(0,100)
+        data.append([batch,[damped_oscillator.x_points[question],damped_oscillator.y_points[question]]])
+        
+    with open('data.json', 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
