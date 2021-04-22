600c326d71577cdf4b4f0dbcb72246de97f423e3
N0WaifuN0Lifu <69238588+N0WaifuN0Lifu@users.noreply.github.com>
2021-04-21 23:53:11 -0600

    Update main.py
    Made more readable

--- Damped_Oscillator/main.py
+++ Damped_Oscillator/main.py
@@ -1,36 +1,38 @@
-import json, argparse, torch, sys
+import json, argparse, torch, sys, pathlib
 import torch.optim as optim
 from torch.autograd import Variable
 import torch.nn.functional as func
 import matplotlib.pyplot as plt
-import seaborn as sns
 import torch.nn as nn
 import numpy as np
 import random
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import cm
-from matplotlib.ticker import LinearLocator, FormatStrFormatter
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-y_list = []
-
-sys.path.append('.//SciNet')
+from pathlib import Path
+PATH = Path(__file__).parent
+sys.path.append(str(PATH.parent / "SciNet"))
+#deals with pathing issues, start from root
 from SciNet_Module import SciNet
-sys.path.append('.//Damped_Oscillator')
 from generator import oscillator  
 
+#Defines device
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+#Y-list must be global so that multiple training sequences can append to it.
+y_list = []
+
+#KL_div function
 def kl_div(mu, sigma):
   kld =  0.5 * torch.mean(torch.sum(mu.pow(2) + (2 * sigma).exp() - 2 * sigma - 1, dim = 1)) 
   return kld
 
 
+#Loss calc function
 def loss_BVAE(y, target, mu, sigma): 
   mseloss = func.mse_loss(y, target)
   kld_loss = kl_div(mu, sigma)
-  #print(mseloss)
   return mseloss, kld_loss
 
 
+#main training function
 def train(net, epoch, learning_rate, beta, data, question, target, batch = 0):
     
     optimizer = optim.Adam(net.parameters(), lr = learning_rate)
@@ -40,7 +42,6 @@ def train(net, epoch, learning_rate, beta, data, question, target, batch = 0):
     x =x.reshape(-1, 50)
     q =q.reshape(-1, 1)
     target = target.reshape(-1, 1)
-    
     if batch:
         for j in range(epoch):
             permutations = torch.randperm(x.shape[0])
@@ -56,23 +57,15 @@ def train(net, epoch, learning_rate, beta, data, question, target, batch = 0):
                 Mseloss, kldloss = loss_BVAE(out, batch_target, mu, sigma)
                 optimizer.zero_grad()
                 net.zero_grad()
-                
-                #print(mu.size())
-                #print(Mseloss)
-                #print(kldloss)
-                
                 loss = (Mseloss + beta * kldloss)
                 tot_loss += loss
-                 
+                
                 loss.backward()
                 torch.nn.utils.clip_grad_value_(net.parameters(), 10)
                 optimizer.step()
                 
             if (j + 1) % 50 == 0:
                 print(j + 1)
-                #print(kldloss)
-                #print(Mseloss)
-                #print(loss)
             y_list.append(tot_loss)
         
     else:
@@ -83,24 +76,17 @@ def train(net, epoch, learning_rate, beta, data, question, target, batch = 0):
         Mseloss, kldloss = loss_BVAE(out, target, mu, sigma)
         optimizer.zero_grad()
         net.zero_grad()
-        
-        #print(mu.size())
-        #print(Mseloss)
-        #print(kldloss)
-    
         loss = (Mseloss + beta * kldloss)
         loss.backward()
         torch.nn.utils.clip_grad_value_(net.parameters(), 10)
         optimizer.step()
         if (i + 1) % 50 == 0:
           print(i + 1)
-          #print(kldloss)
-          #print(Mseloss)
-          #print(loss)
         y_list.append(loss)
 
 def main():
-    samples = 95000
+  #95k comes from paper
+    samples = 100
     data = np.zeros((50, samples))
     q = np.zeros(samples)
     target = np.zeros(samples)
@@ -108,40 +94,31 @@ def main():
     for i in range(samples):
       if ((i+1) % 5000) == 0:
         print(i+1,"out of: ", samples)
-      
+      #defining spring variables
       Gamma = random.uniform(0.5, 1)
       Omega = random.uniform(5, 10)
-      #Velocity = random.uniform(-1, 1)
-      Velocity = 0
-      #print(Gamma, Omega, Alpha)
-      damped_oscillator = oscillator(Gamma, 1, Omega, 0, 10, Velocity, 100)
+      damped_oscillator = oscillator(Gamma, 1, Omega, 0, 10, 0, 100)
       damped_oscillator.iterate_underdamped()
       data[:, i] = np.array(damped_oscillator.y_points[0:50])
-      
       question = random.randint(0, 100)
       q[i] = damped_oscillator.x_points[question]
       target[i] = damped_oscillator.y_points[question]
-    #data = np.array(data)
     
     #Import HyperParams from json
-    with open('params.json') as json_data:
+    with open(PATH/'params.json') as json_data:
       d = json.load(json_data)
       json_data.close()
     learning_rate = d['learning rate']
     num_epochs = d['num epochs']
     beta = d['beta']
-    
-    #Specifies to run on GPU if possible
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    #device = torch.device('cpu')
+
     #Initializing the network
     model = SciNet(observation_size = 50, encode_h1 = 500, encode_h2 = 250, 
                   decoder_input = (1 + 3), decode_h1 = 250, decode_h2 = 100, 
                   output_size = 1, n_latent_var = 3).to(device)
-    
-    #Loss and optimizer
-    #loss = loss_BVAE()
-    
+
+              
+    #call training function
     print("Training Net")
     train(model, 500,learning_rate * 1, beta, data, q, target, 512)
     train(model, 500,learning_rate * 0.1, beta, data, q, target, 512)
@@ -149,26 +126,25 @@ def main():
     plt.cla
     x_list = [i for i in range(0,len(y_list))]
     plt.plot(x_list,y_list)
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
     plt.title("loss")
     plt.show()
     
+    #set up parameters for data testing
     model.eval()
     torch.no_grad()
     testloss = 0
     output= np.zeros(100)
-
-    print("Creating Verification Plot")
+    print("Creating Testing Plot")
+    #range specifies how many testing plot code prints out
     for j in range(3):
       Gamma = random.uniform(0.5,1)
       Omega = random.uniform(5,10)
-      #Velocity = random.uniform(-1,1)
-      Velocity = 0
-      damped_oscillator = oscillator(Gamma, 1, Omega, 0, 10, Velocity, 100)
+      damped_oscillator = oscillator(Gamma, 1, Omega, 0, 10, 0, 100)
       damped_oscillator.iterate_underdamped()
       question = random.randint(0,100)
-      #question = 
       newdata = np.array(damped_oscillator.y_points[0:50])
-
       q = np.array(damped_oscillator.x_points[0:100])
       target = damped_oscillator.y_points[50:100]
       newx = Variable(torch.from_numpy(newdata).to(device,dtype=torch.float32))
@@ -180,36 +156,21 @@ def main():
       values =values.reshape(-1,50)
       q = q.reshape(-1,1)
       for i in range(100):
-
-          #print(q.shape)
-
           out, mu, sigma, _ = model(newx,q[i], 1)
           out = out.cpu()
           out = out.detach().numpy()
-
           output[i] = out[0][0]
 
-          # print(out)
-          # print(np.mean(newdata))
-          # #print(mu)
-          # #print(Gamma)
-          # #print(Omega)
-
+      #Prints Testing plot
       q =q.cpu().detach().numpy()
-
-
       plt.clf
       plt.cla
       plt.title("Output Confirmation Plot")
       plt.plot(damped_oscillator.x_points,damped_oscillator.y_points, color = 'blue')
-      #print(q,output)
       plt.plot(q,output, 'r--')
-      plt.xlabel('Epoch')
-      plt.ylabel('Loss')
-       # plt.scatter(q,target, color = 'black')
       plt.show()
-    
-    
+
+    #sets up scatter plot
     Gamma_list = []
     Velocity_list = []
     latent_out_1 = np.zeros((100,100))
@@ -224,9 +185,7 @@ def main():
             print(j,"/",samples)
           Gamma = k_values[k]
           Omega = w_values[j]
-          #Velocity = random.uniform(-1, 1)
-          Velocity = 0
-          damped_oscillator = oscillator(Gamma, 1, Omega, 0, 10, Velocity, 100)
+          damped_oscillator = oscillator(Gamma, 1, Omega, 0, 10, 0, 100)
           damped_oscillator.iterate_underdamped()
           question = 50
           newdata = np.array(damped_oscillator.y_points[0:50])         
@@ -293,8 +252,6 @@ def main():
     ax.plot_surface(X, Y, latent_out_3, cmap='inferno')
     plt.title("Latent 3")
     plt.show()
-
-
 
 if __name__ == '__main__':
   main()
