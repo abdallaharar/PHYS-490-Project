bcb164844a05e72eaa74c5c91f864f576a7ebd83
Abdallah Arar <abdallahnarar@gmail.com>
2021-04-21 23:42:40 -0400

    fixing relative paths + code cleanup + linting

--- Quantum/main.py
+++ Quantum/main.py
@@ -7,122 +7,100 @@ import torch.nn as nn
 import numpy as np
 from torch.autograd import Variable
 from pathlib import Path
-
 PATH = Path(__file__).parent
+sys.path.append(str(PATH.parent / "SciNet"))
+# sys.path.append('..//SciNet')
+from SciNet_Module import SciNet
+from generator import create_data
 
 # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 device = torch.device( 'cpu')
 
-sys.path.append('..//SciNet')
-from SciNet_Module import SciNet
-
-from generator import create_data
-
 
 def kl_div(mu, sigma):
-    
- 
-  kld =  0.5 * torch.mean(torch.sum(mu.pow(2)+ (2*sigma).exp() -2*sigma -1, dim=1)) 
-  
+  kld = 0.5 * torch.mean(torch.sum(mu.pow(2) +
+                         (2*sigma).exp() - 2*sigma - 1, dim=1))
   return kld
 
 
-def loss_BVAE(y, target, mu, sigma): 
+def loss_BVAE(y, target, mu, sigma):
   mseloss = func.mse_loss((y), target)
-  
+
   kld_loss = kl_div(mu, sigma)
   #print(mseloss)
   return mseloss, kld_loss
 
 
-def train(net, epoch, learning_rate, beta, data, question, target, err,batch=0):
-    
-      
+def train(net, epoch, learning_rate, beta, data, question, target, err, batch=0):
+
     optimizer = optim.Adam(net.parameters(), lr=learning_rate)
-    
-    x = Variable(torch.from_numpy(data).to(device,torch.float32))
-    target =  Variable(torch.from_numpy(target)).to(device,torch.float32)
-    q = Variable(torch.from_numpy(question)).to(device,torch.float32)
-    x =x.reshape(-1,10)
-    q =q.reshape(-1,10)
-    target = target.reshape(-1,1)
-    
+
+    x = Variable(torch.from_numpy(data).to(device, torch.float32))
+    target = Variable(torch.from_numpy(target)).to(device, torch.float32)
+    q = Variable(torch.from_numpy(question)).to(device, torch.float32)
+    x = x.reshape(-1, 10)
+    q = q.reshape(-1, 10)
+    target = target.reshape(-1, 1)
 
     if batch:
         for j in range(epoch):
-            
+
             permutations = torch.randperm(x.shape[0])
             tot_loss = 0
-            
-            for i in range(0,x.shape[0], batch):
-                
+
+            for i in range(0, x.shape[0], batch):
+
                 indices = permutations[i:i+batch]
-                
+
                 batch_x = x[indices]
-                batch_target =  target[indices]
+                batch_target = target[indices]
                 batch_q = q[indices]
-                
-                out, mu, sigma = net(batch_x,batch_q)
+
+                out, mu, sigma = net(batch_x, batch_q)
                 # if j % 1000 == 0 and i % 1000 == 0:
                 #   print("mu:",mu)
                 #   print("sigma:",sigma)
                 Mseloss, kldloss = loss_BVAE(out, batch_target, mu, sigma)
-                
+
                 optimizer.zero_grad()
                 net.zero_grad()
-                
+
                 #print(mu.size())
-                
-                
+
                 #print(Mseloss)
                 #print(kldloss)
-                
-                loss = (Mseloss+ beta* kldloss)
+
+                loss = (Mseloss + beta * kldloss)
                 tot_loss += loss
-                 
+
                 loss.backward()
                 #torch.nn.utils.clip_grad_value_(net.parameters(), 10)
                 optimizer.step()
-                
+
             if (j + 1) % 1 == 0:
                 # print(j)
                 #print(kldloss)
                 #print(Mseloss)
                 # print(tot_loss)
                 err.append(tot_loss.item())
-        
-        
-        
+
     else:
-    
-    
+
       net.train()
       for i in range(epoch):
-      
-        out, mu, sigma = net(x,q)
-    
-    
-        
+
+        out, mu, sigma = net(x, q)
+
         Mseloss, kldloss = loss_BVAE(out, target, mu, sigma)
-        
-        
-        
+
         optimizer.zero_grad()
         net.zero_grad()
-        
-        #print(mu.size())
-        
-        
-        #print(Mseloss)
-        #print(kldloss)
-    
-        loss = (Mseloss+ beta* kldloss)
-    
-     
+
+        loss = (Mseloss + beta * kldloss)
+
         loss.backward()
         torch.nn.utils.clip_grad_value_(net.parameters(), 10)
-             
-      
+
         optimizer.step()
         if (i + 1) % 50 == 0:
           print(i+1)
@@ -134,24 +112,21 @@ def train(net, epoch, learning_rate, beta, data, question, target, err,batch=0):
         
 def test(model, test_set):
 
- 
-    
-    
     observations = test_set[0]
-    question =test_set[1]
+    question = test_set[1]
     target = test_set[2]
-    
-    x = Variable(torch.from_numpy(observations).to(device,torch.float32))
-    target =  Variable(torch.from_numpy(target)).to(device,torch.float32)
-    q = Variable(torch.from_numpy(question)).to(device,torch.float32)
-    x =x.reshape(-1,10)
-    q =q.reshape(-1,10)
-    target = target.reshape(-1,1)
-    
-    out, mu, sigma = model(x,q)
-    
+
+    x = Variable(torch.from_numpy(observations).to(device, torch.float32))
+    target = Variable(torch.from_numpy(target)).to(device, torch.float32)
+    q = Variable(torch.from_numpy(question)).to(device, torch.float32)
+    x = x.reshape(-1, 10)
+    q = q.reshape(-1, 10)
+    target = target.reshape(-1, 1)
+
+    out, mu, sigma = model(x, q)
+
     Mseloss, kldloss = loss_BVAE(out, target, mu, sigma)
-    
+
     return Mseloss
     
     
@@ -172,14 +147,9 @@ def plot_paper_results():
     incompl_set_test = (a[0][:test_samples],a[1][:test_samples], a[2][:test_samples])
     
     #Import HyperParams from json
-    with open('params.json') as json_data:
+    with open(PATH / 'params.json') as json_data:
       d = json.load(json_data)
       json_data.close()
-    learning_rate = d['learning rate1']
-    learning_rate2 = d['learning rate2']
-    num_epochs1 = d['num epochs1']
-    mum_epochs2 = d['num epochs2']
-    beta = d['beta']
     latent_max = d['latent max']+1
 
     # plot complete tomography
@@ -210,8 +180,10 @@ def plot_paper_results():
       model.eval()
       error_i[k] = test(model, incompl_set_test)
     
-    p1 = plt.bar(np.arange(0,latent_max),np.sqrt(error_c), color ='blue', width = 0.8,align = "center")
-    p2 = plt.bar(np.arange(0,latent_max),np.sqrt(error_i), color ='orange', width = 0.4, align = "edge")
+    p1 = plt.bar(np.arange(0, latent_max), np.sqrt(error_c),
+                 color='blue', width=0.8, align="center")
+    p2 = plt.bar(np.arange(0, latent_max), np.sqrt(error_i),
+                 color='orange', width=0.4, align="edge")
     plt.ylabel("Error")
     plt.title("One qubit.")
     plt.xlabel("Number of latent neurons")
@@ -245,13 +217,13 @@ def main():
                     
         
     #Import HyperParams from json
-    with open('params.json') as json_data:
+    with open(PATH / 'params.json') as json_data:
       d = json.load(json_data)
       json_data.close()
-    learning_rate = d['learning rate1']
+    learning_rate1 = d['learning rate1']
     learning_rate2 = d['learning rate2']
     num_epochs1 = d['num epochs1']
-    mum_epochs2 = d['num epochs2']
+    num_epochs2 = d['num epochs2']
     beta = d['beta']
     latent_max = d['latent max']+1
     
@@ -270,9 +242,9 @@ def main():
         #Loss and optimizer
         #loss = loss_BVAE()
         err = []
-        train(model, num_epochs1,learning_rate, beta, observations, question,
+        train(model, num_epochs1, learning_rate1, beta, observations, question,
               target, err, 512)
-        train(model, mum_epochs2,learning_rate2, beta, observations, question,
+        train(model, num_epochs2,learning_rate2, beta, observations, question,
               target,err, 512)
         plt.clf
         plt.cla
